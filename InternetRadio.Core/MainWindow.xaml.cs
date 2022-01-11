0e97cc4c875c7e2d68c884a0f5d0029215003e02
FloH <doc.nice@gmx.de>
2022-01-11 21:23:38 +0100

    Minimal Repro: MedieElement fails on HTTPS
    When `MediaElement.Source` is set to a HTTPS-Uri
    - .NET 4.8 Project throws `NullReferenceException`
       at System.Windows.Media.MediaPlayerState.OpenMedia(Uri source)
       at System.Windows.Media.MediaPlayerState.SetSource(Uri source)
       at System.Windows.Media.MediaPlayerState.Open(Uri source)
    - .NET Core 5.0 Application sucessfully plays the Source.
    
    Plain HTTP works in both runtimes.
    
    See
    https://stackoverflow.com/questions/30702505/playing-media-from-https-site-in-media-element-throwing-null-reference-exception
    https://github.com/dotnet/wpf/issues/722
    fixed and merged with https://github.com/dotnet/wpf/pull/969
    into .NET Core.

---
+++ InternetRadio.Core/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InternetRadio.Core
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Player.LoadedBehavior = MediaState.Manual;
+            this.Player.UnloadedBehavior = MediaState.Manual;
+        }
+
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            this.Player.Stop();
+            this.Player.Position = TimeSpan.Zero;
+
+            try
+            {
+                string source = ((Button)sender).CommandParameter as string;
+                this.Player.Source = new Uri(source, UriKind.Absolute);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Player.Play:\n" + ex);
+            }
+
+            try
+            {
+                this.Player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Player.Play:\n" + ex);
+            }
+        }
+    }
+}
